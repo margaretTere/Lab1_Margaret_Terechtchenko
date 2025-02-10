bfc5fd6aa758f1866b3d4261b3f409265566bccb
margaretTere <may.terechenko@gmail.com>
2025-02-09 21:48:24 -0500

    Added text element for random number

--- Lab1_Margaret_Terechtchenko/Lab1_Margaret_Terechtchenko/ContentView.swift
+++ Lab1_Margaret_Terechtchenko/Lab1_Margaret_Terechtchenko/ContentView.swift
@@ -8,17 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomNumber: Int = -1
     var body: some View {
         ZStack {
             VStack {
+                
+                Text("\(randomNumber)")
+                      .font(.system(size: 50))
+                      .fontWeight(.bold)
+                      .frame(width: 200, height: 70)
+                      .background(Color.white)
+                      .foregroundColor(.green)
+                      .cornerRadius(10)
+                
                 Image(systemName: "globe")
                     .imageScale(.large)
                     .foregroundStyle(.tint)
                 Text("Hello, world!")
             }
             .padding()
+            .onAppear {
+                generateNewNumber()
+            }
         }
     }
+    
+    func generateNewNumber(){
+         randomNumber = Int.random(in: 1...100)
+     }
 }
 
 func isPrime(_ number: Int) -> Bool {
